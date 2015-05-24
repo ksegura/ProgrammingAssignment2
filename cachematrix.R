608d15643496d2578c592d97d921e16721bda46d
Kelvy Segura Cuevas <ksegurac@gmail.com>
2015-05-24 10:32:02 -0400

    2do commit

---
+++ cachematrix.R
@@ -0,0 +1,47 @@
+
+# ****************************************************************
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL 
+
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function(){ x }
+  
+  setinv <- function(i){ m <<- i }
+  
+  getinv <- function(){ m }  
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)    
+}
+
+# ****************************************************************
+# Instale el paquete MASS con : 
+#                               install.packages("MASS",dep=TRUE)
+# Cargar el paquete MASS con : 
+#                              library(MASS)
+#
+# ****************************************************************
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("Obteniendo los datos en cache")    
+  }else{
+    data <- x$get()
+    m <- ginv(data, ...)
+    x$setinv(m)
+  }
+  return(m)  
+}
+
+# ************************************************************
+#  Ejemplo
+# ************************************************************
+
+# matriz <- makeCacheMatrix(matrix(rnorm(25)*10,5,5))
+# cacheSolve(matriz)
+# cacheSolve(matriz)
